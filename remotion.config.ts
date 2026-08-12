30d1b884f7d08b22714fdea2bde06bc971ba6fd6
sunnybaloch1110-wq <sunnybaloch1110@gmail.com>
2026-08-12 11:23:28 -0700

    Enable Linux Chromium multi-process rendering

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,5 +1,6 @@
 import { Config } from '@remotion/cli/config';
 
+Config.setChromiumMultiProcessOnLinux(true);
 Config.setVideoImageFormat('jpeg');
 Config.setPixelFormat('yuv420p');
 Config.setOverwriteOutput(true);
