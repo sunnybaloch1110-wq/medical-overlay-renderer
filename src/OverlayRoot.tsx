31337754980d3fad26f095d6bc9a0d053e4d4074
sunnybaloch1110-wq <sunnybaloch1110@gmail.com>
2026-08-22 22:52:56 -0700

    Route all external visual types through dynamic overlay engine

--- src/OverlayRoot.tsx
+++ src/OverlayRoot.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { Composition } from 'remotion';
 import { VIDEO_DEFAULTS } from './config/video';
-import { resolveRenderer } from './renderers';
+import { DynamicOverlay } from './DynamicOverlay';
 import type { OverlayRenderProps } from './types';
 
 export const OverlayRoot: React.FC = () => (
   <Composition<OverlayRenderProps>
     id="MedicalOverlay"
-    component={resolveRenderer('blood_flow')}
+    component={DynamicOverlay}
     durationInFrames={1}
     fps={VIDEO_DEFAULTS.fps}
     width={VIDEO_DEFAULTS.width}
     height={VIDEO_DEFAULTS.height}
     defaultProps={{ durationInSeconds: 1, visualType: 'blood_flow' }}
     calculateMetadata={({ props }) => ({
-      durationInFrames: Math.max(1, Math.round(props.durationInSeconds * VIDEO_DEFAULTS.fps)),
-      fps: VIDEO_DEFAULTS.fps,
-      width: VIDEO_DEFAULTS.width,
-      height: VIDEO_DEFAULTS.height,
-    })}
-    resolveMetadata={({ props }) => ({
       durationInFrames: Math.max(1, Math.round(props.durationInSeconds * VIDEO_DEFAULTS.fps)),
       fps: VIDEO_DEFAULTS.fps,
       width: VIDEO_DEFAULTS.width,
