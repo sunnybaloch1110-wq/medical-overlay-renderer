331aef2893ae6e63158bf8c711d7ebf525780e38
sunnybaloch1110-wq <sunnybaloch1110@gmail.com>
2026-08-22 22:52:22 -0700

    Make Remotion composition duration input-driven

--- src/OverlayRoot.tsx
+++ src/OverlayRoot.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Composition } from 'remotion';
-import { VIDEO } from './config/video';
+import { VIDEO_DEFAULTS } from './config/video';
 import { BloodFlowOverlay } from './components/BloodFlowOverlay';
 
 export type OverlayRenderProps = {
@@ -14,15 +14,18 @@ export type OverlayRenderProps = {
 
 export const OverlayRoot: React.FC = () => (
   <Composition<OverlayRenderProps>
-    id="MedicalBloodFlow"
+    id="MedicalOverlay"
     component={BloodFlowOverlay}
-    durationInFrames={VIDEO.durationInFrames}
-    fps={VIDEO.fps}
-    width={VIDEO.width}
-    height={VIDEO.height}
-    defaultProps={{durationInSeconds: VIDEO.durationInSeconds}}
-    calculateMetadata={({props}) => ({
-      durationInFrames: Math.max(1, Math.round(props.durationInSeconds * VIDEO.fps)),
+    durationInFrames={1}
+    fps={VIDEO_DEFAULTS.fps}
+    width={VIDEO_DEFAULTS.width}
+    height={VIDEO_DEFAULTS.height}
+    defaultProps={{ durationInSeconds: 1 }}
+    calculateMetadata={({ props }) => ({
+      durationInFrames: Math.max(1, Math.round(props.durationInSeconds * VIDEO_DEFAULTS.fps)),
+      fps: VIDEO_DEFAULTS.fps,
+      width: VIDEO_DEFAULTS.width,
+      height: VIDEO_DEFAULTS.height,
     })}
   />
 );
