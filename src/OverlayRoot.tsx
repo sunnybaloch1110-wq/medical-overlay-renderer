4391ef7d3ccfa52e7d90472472bb41766a9f9185
sunnybaloch1110-wq <sunnybaloch1110@gmail.com>
2026-08-22 22:52:46 -0700

    Make composition independent of daily video content

--- src/OverlayRoot.tsx
+++ src/OverlayRoot.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Composition } from 'remotion';
 import { VIDEO_DEFAULTS } from './config/video';
-import { BloodFlowOverlay } from './components/BloodFlowOverlay';
-
-export type OverlayRenderProps = {
-  durationInSeconds: number;
-  id?: string;
-  concept?: string;
-  transcript?: string;
-  renderer?: string;
-  props?: Record<string, unknown>;
-};
+import { resolveRenderer } from './renderers';
+import type { OverlayRenderProps } from './types';
 
 export const OverlayRoot: React.FC = () => (
   <Composition<OverlayRenderProps>
     id="MedicalOverlay"
-    component={BloodFlowOverlay}
+    component={resolveRenderer('blood_flow')}
     durationInFrames={1}
     fps={VIDEO_DEFAULTS.fps}
     width={VIDEO_DEFAULTS.width}
     height={VIDEO_DEFAULTS.height}
-    defaultProps={{ durationInSeconds: 1 }}
+    defaultProps={{ durationInSeconds: 1, visualType: 'blood_flow' }}
     calculateMetadata={({ props }) => ({
+      durationInFrames: Math.max(1, Math.round(props.durationInSeconds * VIDEO_DEFAULTS.fps)),
+      fps: VIDEO_DEFAULTS.fps,
+      width: VIDEO_DEFAULTS.width,
+      height: VIDEO_DEFAULTS.height,
+    })}
+    resolveMetadata={({ props }) => ({
       durationInFrames: Math.max(1, Math.round(props.durationInSeconds * VIDEO_DEFAULTS.fps)),
       fps: VIDEO_DEFAULTS.fps,
       width: VIDEO_DEFAULTS.width,
