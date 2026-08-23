1687da0e1cbc7524443b3b9b190ad3a343575268
sunnybaloch1110-wq <sunnybaloch1110@gmail.com>
2026-08-23 00:34:32 -0700

    Normalize generic composition after POC removal

--- src/OverlayRoot.tsx
+++ src/OverlayRoot.tsx
@@ -12,10 +12,7 @@ export const OverlayRoot: React.FC = () => (
     fps={VIDEO_DEFAULTS.fps}
     width={VIDEO_DEFAULTS.width}
     height={VIDEO_DEFAULTS.height}
-    defaultProps={{
-      durationInSeconds: 1,
-      scene: { elements: [] },
-    }}
+    defaultProps={{ durationInSeconds: 1, scene: { elements: [] } }}
     calculateMetadata={({ props }) => ({
       durationInFrames: Math.max(1, Math.round(props.durationInSeconds * VIDEO_DEFAULTS.fps)),
       fps: VIDEO_DEFAULTS.fps,
